dc5d9eb82673c9626f1d4ec5aa95b1bd280c63be
Tulir Asokan <tulir@maunium.net>
2020-10-01 12:18:43 +0300

    Remove automatic permission changing in Docker image

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-# Define functions.
-function fixperms {
-	chown -R $UID:$GID /data /opt/mautrix-signal
-}
-
 cd /opt/mautrix-signal
 
 if [ ! -f /data/config.yaml ]; then
@@ -13,7 +7,6 @@ if [ ! -f /data/config.yaml ]; then
 	echo "Copied default config file to /data/config.yaml"
 	echo "Modify that config file to your liking."
 	echo "Start the container again after that to generate the registration file."
-	fixperms
 	exit
 fi
 
@@ -22,9 +15,7 @@ if [ ! -f /data/registration.yaml ]; then
 	echo "Didn't find a registration file."
 	echo "Generated one for you."
 	echo "Copy that over to synapses app service directory."
-	fixperms
 	exit
 fi
 
-fixperms
-exec su-exec $UID:$GID python3 -m mautrix_signal -c /data/config.yaml
+exec python3 -m mautrix_signal -c /data/config.yaml
