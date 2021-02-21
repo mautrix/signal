9ae4e7e4ab53db4efe0ad4238c192c37e974b563
Tulir Asokan <tulir@maunium.net>
2021-02-21 17:24:35 +0200

    Let mautrix-python handle registration generation message

--- docker-run.sh
+++ docker-run.sh
@@ -12,9 +12,6 @@ fi
 
 if [ ! -f /data/registration.yaml ]; then
 	python3 -m mautrix_signal -g -c /data/config.yaml -r /data/registration.yaml
-	echo "Didn't find a registration file."
-	echo "Generated one for you."
-	echo "Copy that over to synapses app service directory."
 	exit
 fi
 
