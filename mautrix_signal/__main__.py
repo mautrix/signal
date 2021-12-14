a7095c737dac18cbbf2d52d8add7633caf9a20b6
Sumner Evans <me@sumnerevans.com>
2021-12-14 14:42:02 -0700

    convert to use new double-puppeting key

--- mautrix_signal/__main__.py
+++ mautrix_signal/__main__.py
@@ -41,7 +41,7 @@ class SignalBridge(Bridge):
     command = "python -m mautrix-signal"
     description = "A Matrix-Signal puppeting bridge."
     repo_url = "https://github.com/mautrix/signal"
-    real_user_content_key = "net.maunium.signal.puppet"
+    bridge_name = "signal"
     version = version
     markdown_version = linkified_version
     config_class = Config
