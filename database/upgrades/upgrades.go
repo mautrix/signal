9e012e9ca7e620083a66353a59dfd9d6f7811764
Scott Weber <scott@beeper.com>
2023-09-11 15:53:53 -0400

    Fix DB upgrading

--- database/upgrades/upgrades.go
+++ database/upgrades/upgrades.go
@@ -18,6 +18,7 @@ package upgrades
 
 import (
 	"embed"
+	"errors"
 
 	"maunium.net/go/mautrix/util/dbutil"
 )
@@ -28,5 +29,11 @@ var Table dbutil.UpgradeTable
 var rawUpgrades embed.FS
 
 func init() {
+	Table.Register(-1, 12, 0, "Unsupported version", false, func(tx dbutil.Execable, database *dbutil.Database) error {
+		return errors.New("please upgrade to mautrix-signal v0.4.3 before upgrading to a newer version")
+	})
+	Table.Register(1, 12, 0, "Jump to version 12", false, func(tx dbutil.Execable, database *dbutil.Database) error {
+		return nil
+	})
 	Table.RegisterFS(rawUpgrades)
 }
