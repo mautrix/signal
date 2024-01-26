mautrix/signal

commit 666ccf11fba059baabbe98f870d1014340a0ad91

Ignore duplicate reactions from Signal (ref #431)

portal.go
@@ -937,6 +937,9 @@ func (portal *Portal) handleSignalReaction(sender *Puppet, react *signalpb.DataM
 	if err != nil {
 		log.Err(err).Msg("Failed to get existing reaction from database")
 		return
+	} else if existingReaction.Emoji == react.GetEmoji() {
+		log.Debug().Msg("Ignoring duplicate reaction")
+		return
 	}
 	intent := sender.IntentFor(portal)
 	if existingReaction != nil {

commit f5468a6e8c79eddd0337bb5f06a847a7d6dbc291

Avoid nil dereference on existing reaction lookup (#433)
Otherwise, the bridge crashes when it sees a reaction from Signal.

---

This fixes a crash introduced by 666ccf1.

portal.go
@@ -937,7 +937,7 @@ func (portal *Portal) handleSignalReaction(sender *Puppet, react *signalpb.DataM
 	if err != nil {
 		log.Err(err).Msg("Failed to get existing reaction from database")
 		return
-	} else if existingReaction.Emoji == react.GetEmoji() {
+	} else if existingReaction != nil && existingReaction.Emoji == react.GetEmoji() {
 		log.Debug().Msg("Ignoring duplicate reaction")
 		return
 	}