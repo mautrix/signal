7eeea39ddc55662d81ba9298bc6c1fb16cba14ab
Andrew Ferrazzutti <andrewf@element.io>
2024-02-05 08:35:36 -0500

    Don't omit debug info on debug build

--- build-go.sh
+++ build-go.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | awk '{ print $2 }')
-GO_LDFLAGS="-s -w -X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date '+%b %_d %Y, %H:%M:%S'`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'"
+GO_LDFLAGS="-X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date '+%b %_d %Y, %H:%M:%S'`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'"
 if [[ $DBG -eq 1 ]]; then
     GO_GCFLAGS='all=-N -l'
+else
+    GO_LDFLAGS="-s -w ${GO_LDFLAGS}"
 fi
 go build -gcflags="$GO_GCFLAGS" -ldflags="$GO_LDFLAGS" -o mautrix-signal
