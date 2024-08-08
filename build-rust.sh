f3a286f68a91ef593bccb71db433472c7d8072f4
Tulir Asokan <tulir@maunium.net>
2024-08-09 01:59:13 +0300

    build: always update submodules
    [skip ci]

--- build-rust.sh
+++ build-rust.sh
@@ -6,4 +6,5 @@
 #    RUST_PROFILE=dev
 #fi
 RUST_PROFILE=release
+git submodule update --init
 cd pkg/libsignalgo/libsignal && RUSTFLAGS="-Ctarget-feature=-crt-static" RUSTC_WRAPPER="" cargo build -p libsignal-ffi --profile=$RUST_PROFILE
