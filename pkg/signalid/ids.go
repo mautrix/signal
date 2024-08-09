06fcd2d352ee8412f77b46059a263f14edc04c41
Tulir Asokan <tulir@maunium.net>
2024-08-09 23:41:21 +0300

    Apply suggestions from code review

--- pkg/signalid/ids.go
+++ pkg/signalid/ids.go
@@ -40,7 +40,7 @@ func ParseUserID(userID networkid.UserID) (uuid.UUID, error) {
 }
 
 func ParseUserLoginID(userLoginID networkid.UserLoginID) (uuid.UUID, error) {
-	userID, err := ParseUserLoginID(userLoginID)
+	userID, err := uuid.Parse(string(userLoginID))
 	if err != nil {
 		return uuid.Nil, err
 	}
@@ -49,10 +49,6 @@ func ParseUserLoginID(userLoginID networkid.UserLoginID) (uuid.UUID, error) {
 
 func ParseUserIDAsServiceID(userID networkid.UserID) (libsignalgo.ServiceID, error) {
 	return libsignalgo.ServiceIDFromString(string(userID))
-}
-
-func ParseUserLoginIDAsServiceID(userLoginID networkid.UserLoginID) (libsignalgo.ServiceID, error) {
-	return libsignalgo.ServiceIDFromString(string(userLoginID))
 }
 
 func ParsePortalID(portalID networkid.PortalID) (userID libsignalgo.ServiceID, groupID types.GroupIdentifier, err error) {
