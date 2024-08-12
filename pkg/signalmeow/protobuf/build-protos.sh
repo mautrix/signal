3c483d16f372abd8bfd082ea66c430e12ad45f94
Tulir Asokan <tulir@maunium.net>
2024-08-13 01:23:18 +0300

    signalmeow: use go:embed for protobuf compiling

--- pkg/signalmeow/protobuf/build-protos.sh
+++ pkg/signalmeow/protobuf/build-protos.sh
@@ -1,17 +1,11 @@
-#!/usr/bin/env bash
-
-# This script is used to generate the protobuf files for the project.
-# It is assumed that the protoc compiler is installed and available on the path.
-
-# The script will generate the protobuf files for the following languages:
-# - Go
-
+#!/bin/sh
 PKG_IMPORT_PATH="go.mau.fi/mautrix-signal/pkg/signalmeow/signalpb"
-
 for file in *.proto
 do
-    protoc --go_out=. \
-    --go_opt=M${file}=$PKG_IMPORT_PATH \
-    --go_opt=paths=source_relative \
-    $file
+	# Requires https://go-review.googlesource.com/c/protobuf/+/369634
+	protoc --go_out=. \
+		--go_opt=M${file}=$PKG_IMPORT_PATH \
+		--go_opt=paths=source_relative \
+		--go_opt=embed_raw=true \
+		$file
 done
