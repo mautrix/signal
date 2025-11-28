079223eed63b39f10978972ca689b5641813ee33
Tulir Asokan <tulir@maunium.net>
2025-11-28 16:18:45 +0200

    signalmeow/store: fix latest version number

--- pkg/signalmeow/store/upgrades/24-outbound-sender-keys.sql
+++ pkg/signalmeow/store/upgrades/24-outbound-sender-keys.sql
@@ -1,5 +1,5 @@
 -- v24 (compatible with v13+): Store outbound sender keys for groups
-CREATE TABLE signalmeow_outbound_sender_key_info (
+CREATE TABLE IF NOT EXISTS signalmeow_outbound_sender_key_info (
     account_id      TEXT  NOT NULL,
     group_id        TEXT  NOT NULL,
     distribution_id TEXT  NOT NULL,
