28c03d8e0e69566d6339578a706441841d7e0ec1
Tulir Asokan <tulir@maunium.net>
2025-12-21 23:34:31 +0200

    libsignalgo/sealedsender: remove unnecessary debug code

--- pkg/libsignalgo/sealedsender.go
+++ pkg/libsignalgo/sealedsender.go
@@ -24,14 +24,10 @@ import "C"
 import (
 	"context"
 	"fmt"
-	"maps"
 	"runtime"
-	"slices"
 	"unsafe"
 
 	"github.com/google/uuid"
-	"github.com/rs/zerolog"
-	"go.mau.fi/util/exerrors"
 )
 
 type SealedSenderAddress struct {
@@ -103,29 +99,9 @@ func SealedSenderMultiRecipientEncrypt(
 	recipientAddresses := make([]C.SignalConstPointerProtocolAddress, len(recipients))
 	recipientSessions := make([]C.SignalConstPointerSessionRecord, len(recipients))
 
-	type dedupTuple struct {
-		Name     string
-		DeviceID uint
-	}
-	deviceDedup := make(map[dedupTuple]struct{})
-	var duplicateFound *dedupTuple
-
 	for i, recipient := range recipients {
-		name := exerrors.Must(recipient.Address.Name())
-		deviceID := exerrors.Must(recipient.Address.DeviceID())
-		dedupKey := dedupTuple{name, deviceID}
-		if _, exists := deviceDedup[dedupKey]; exists {
-			duplicateFound = &dedupKey
-		}
 		recipientAddresses[i] = recipient.Address.constPtr()
 		recipientSessions[i] = recipient.Record.constPtr()
-	}
-	if duplicateFound != nil {
-		zerolog.Ctx(ctx).Debug().
-			Any("full_list", slices.Collect(maps.Keys(deviceDedup))).
-			Any("last_duplicate", *duplicateFound).
-			Msg("Duplicate debug data")
-		return nil, fmt.Errorf("duplicate recipient addresses found in SealedSenderMultiRecipientEncrypt")
 	}
 	signalFfiError := C.signal_sealed_sender_multi_recipient_encrypt(
 		&encrypted,
