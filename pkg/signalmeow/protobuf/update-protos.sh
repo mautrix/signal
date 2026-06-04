04fc9acaf4ffb7c469c70eaba456474aeae37a3e
Tulir Asokan <tulir@maunium.net>
2026-06-05 00:12:12 +0300

    signalmeow: switch to fetching cds2.proto from libsignal

--- pkg/signalmeow/protobuf/update-protos.sh
+++ pkg/signalmeow/protobuf/update-protos.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 ANDROID_GIT_REVISION=${1:-aa9591211ba0c77376318bdd5f014e064b8e8de4}
-DESKTOP_GIT_REVISION=${1:-a0af83d7488930c213a7b6dd554490ebe9e65628}
+DESKTOP_GIT_REVISION=${2:-a0af83d7488930c213a7b6dd554490ebe9e65628}
+LIBSIGNAL_GIT_REVISION=${3:-46d867c986f66201e34e7ae20ce423eec742bf3f}
 
 update_proto() {
   case "$1" in
@@ -26,6 +27,11 @@ update_proto() {
       prefix="protos/"
       GIT_REVISION=$DESKTOP_GIT_REVISION
       ;;
+    libsignal)
+      REPO="libsignal"
+      prefix="rust/net/src/proto/"
+      GIT_REVISION=$LIBSIGNAL_GIT_REVISION
+      ;;
   esac
   echo https://raw.githubusercontent.com/signalapp/${REPO}/${GIT_REVISION}/${prefix}${2}
   curl -LOf https://raw.githubusercontent.com/signalapp/${REPO}/${GIT_REVISION}/${prefix}${2}
@@ -43,6 +49,7 @@ update_proto Signal-Android-Archive Backup.proto
 mv Backup.proto backuppb/Backup.proto
 
 update_proto Signal-Desktop DeviceName.proto
+update_proto libsignal cds2.proto
+mv cds2.proto cds2pb/cds2.proto
 # TODO these were moved to libsignal only
 #update_proto Signal-Desktop UnidentifiedDelivery.proto
-#update_proto Signal-Desktop ContactDiscovery.proto
